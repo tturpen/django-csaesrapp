b6bcfc054ddb0aa2ce5f01e7fc5e3e5652031c6c
tturpen <taylor.turpen@gmail.com>
2014-03-21 16:25:47 -0400

    Updates util tests.

--- apps/util/tests.py
+++ apps/util/tests.py
@@ -14,3 +14,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+#     def test_cer_wer(self):
+#         ref = ["Humpty","dumpty","broke","his","head."]
+#         hyp = ["Humpty","dumpty","broke","his","nose",""]#"on","the","sidewalk","by","the","church."]
+#         
+#         ref2 = ['is', "lockwood's", 'destination', 'the', 'same', 'as', 'the', "mcclusky's"]
+#         hyp2 = ['is', 'lockwoods', 'destination', 'the', 'same', 'as', 'the', 'macloskeys']
+#         
+#         hyp3 = ['list', 'confidences', 'threats.']
+#         ref3 = ['list', "confidence's", 'threats']
+#         
+#         print(cer_wer(ref2,hyp2))
+#         print(wer(ref,hyp))
