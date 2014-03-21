862b5ee6ad96192cf44823eb94c9812e9df96b70
tturpen <taylor.turpen@gmail.com>
2014-03-21 16:24:40 -0400

    Updates elicitation handlers.

--- apps/elicitation/handlers.py
+++ apps/elicitation/handlers.py
@@ -1,16 +1,17 @@
 from apps.common.handlers import ModelHandler
-from apps.common.models import (PromptSource, 
-                                ResourceManagementPrompt, 
-                                ResourceManagementAudioSource,
-                                ElicitationAudioRecording,
-                                AudioClip,
-                                Transcription,
-                                AudioClipQueue,
-                                PromptQueue,
-                                ElicitationHit,
-                                TranscriptionHit,
-                                TranscriptionAssignment,
-                                ElicitationAssignment)
+from apps.common.models import PromptSource
+from apps.mturk.handlers import AssignmentHandler, TurkerHandler, HitHandler
+from apps.elicitation.models import (ResourceManagementPrompt, 
+                                    ElicitationAudioRecording,
+                                    PromptQueue,
+                                    ElicitationHit,
+                                    ElicitationAssignment)
+from apps.common.handlers import PipelineHandler
+from apps.text.handlers import PromptHandler
+from apps.normalization.handlers import NormalizationHandler
+from apps.filtering.handlers import StandardFilterHandler
+
+import logging
 
 class ElicitationModelHandler(ModelHandler):
     
@@ -23,5 +24,15 @@ class ElicitationModelHandler(ModelHandler):
         self.c["assignments"] = ElicitationAssignment.objects
  
         self.logger = logging.getLogger("transcription_engine.mongodb_elicitation_handler")
+        
+
+class ElicitationPipelineHandler(PipelineHandler):
+    
+    def __init__(self):
+        PipelineHandler.__init__(self)
+        self.mh = ElicitationModelHandler()
+        self.ph = PromptHandler()
+        self.filter = StandardFilterHandler(self.mh)
+        self.logger = logging.getLogger("transcription_engine.elicitation_pipeline_handler")
 
     
