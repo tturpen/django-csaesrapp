e0b43f23ae934afa3141a392d636990b84eb2256
tturpen <taylor.turpen@gmail.com>
2014-03-21 16:25:34 -0400

    Updates transcription handlers and views.

--- apps/transcription/handlers.py
+++ apps/transcription/handlers.py
@@ -1,3 +1,22 @@
+"""Specific transcription pipeline handlers"""
+# Copyright (C) 2014 Taylor Turpen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import logging
+import os
+import datetime
+import time
+
 from apps.transcription.models import (ResourceManagementAudioSource,
                                 AudioClip,
                                 Transcription,
@@ -7,6 +26,7 @@ from apps.transcription.models import (ResourceManagementAudioSource,
 
 from apps.common.handlers import ModelHandler
 import logging
+import os
 
 class TranscriptionModelHandler(ModelHandler):
     
@@ -21,5 +41,4 @@ class TranscriptionModelHandler(ModelHandler):
         self.c["assignments"] = TranscriptionAssignment.objects
  
         self.logger = logging.getLogger("csaesr.%s"%self.__name__)
-        
         
